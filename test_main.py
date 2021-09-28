59939f71fff4c13b6790fd1a3559e7a1dc458f5a
Elliana May <me@mause.me>
2021-09-28 11:19:55 +0000

    Fix types

--- test_main.py
+++ test_main.py
@@ -1,24 +1,26 @@
 from base64 import b64encode
+from typing import Generator
 
 import pytest
+from flask.testing import FlaskClient
 
 from main import app
 
 
-@pytest.fixture
-def client():
+@pytest.fixture  # type: ignore
+def client() -> Generator[FlaskClient, None, None]:
     app.config["TESTING"] = True
 
     yield app.test_client()
 
 
-def test_thing(client):
+def test_thing(client: FlaskClient) -> None:
     response = client.post("/hook", json={"service": "animelab", "show": "Black"})
 
     assert response.status == "200 OK"
 
 
-def test_redis(client):
+def test_redis(client: FlaskClient) -> None:
     response = client.get("/redis")
 
     assert response.status == "401 UNAUTHORIZED"
